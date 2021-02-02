ad7c0e74d85a2a683c931016f17158bdaa51383d
Ryan Esterhuysen <ryanesterh3@gmail.com>
2021-02-02 15:04:21 +0200

    Extracted methods CalculateAmount and CalculateFrequentRenterPoints and cleaned up Statement method a bit

--- MovieRental/Customer.cs
+++ MovieRental/Customer.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace MovieRental
 {
@@ -21,64 +22,24 @@ namespace MovieRental
 
         public string Statement()
         {
-            double totalAmount = 0;
-            int frequentRenterPoints = 0;
-            
             string result = "Rental Record for " + Name + "\n";
 
             foreach (var rental in _rentals)
             {
-                double thisAmount = CalculateAmount(rental);
-
-                frequentRenterPoints += CalculateFrequentRenterPoints(rental);
+                double thisAmount = rental.CalculateAmount();
 
                 // show figures for this rental
                 result += "\t" + rental.Movie.Title + "\t" + thisAmount + "\n";
-                
-                totalAmount += thisAmount;
             }
+
+            double totalAmount = _rentals.Sum(r => r.CalculateAmount());
+            int frequentRenterPoints = _rentals.Sum(r => r.CalculateFrequentRenterPoints());
 
             // add footer lines
             result += "Amount owed is " + totalAmount + "\n";
             result += "You earned " + frequentRenterPoints + " frequent renter points";
 
             return result;
-        }
-
-        private int CalculateFrequentRenterPoints(Rental rental)
-        {
-            int frequentRenterPoints = 1;
-
-            // add bonus for a two day new release rental
-            if (rental.Movie.PriceCode == Movie.NewRelease && rental.DaysRented > 1)
-                frequentRenterPoints++;
-            
-            return frequentRenterPoints;
-        }
-
-        private double CalculateAmount(Rental rental)
-        {
-            double thisAmount = 0.00;
-            
-            //determine amounts for each line
-            switch (rental.Movie.PriceCode)
-            {
-                case Movie.Regular:
-                    thisAmount += 2;
-                    if (rental.DaysRented > 2)
-                        thisAmount += (rental.DaysRented - 2) * 1.5;
-                    break;
-                case Movie.NewRelease:
-                    thisAmount += rental.DaysRented * 3;
-                    break;
-                case Movie.Childrens:
-                    thisAmount += 1.5;
-                    if (rental.DaysRented > 3)
-                        thisAmount += (rental.DaysRented - 3) * 1.5;
-                    break;
-            }
-
-            return thisAmount;
         }
     }
 }
--- MovieRental/Rental.cs
+++ MovieRental/Rental.cs
@@ -11,5 +11,40 @@ namespace MovieRental
             DaysRented = daysRented;
         }
 
+        public int CalculateFrequentRenterPoints()
+        {
+            int frequentRenterPoints = 1;
+
+            // add bonus for a two day new release rental
+            if (this.Movie.PriceCode == Movie.NewRelease && this.DaysRented > 1)
+                frequentRenterPoints++;
+            
+            return frequentRenterPoints;
+        }
+
+        public double CalculateAmount()
+        {
+            double thisAmount = 0.00;
+            
+            //determine amounts for each line
+            switch (this.Movie.PriceCode)
+            {
+                case Movie.Regular:
+                    thisAmount += 2;
+                    if (this.DaysRented > 2)
+                        thisAmount += (this.DaysRented - 2) * 1.5;
+                    break;
+                case Movie.NewRelease:
+                    thisAmount += this.DaysRented * 3;
+                    break;
+                case Movie.Childrens:
+                    thisAmount += 1.5;
+                    if (this.DaysRented > 3)
+                        thisAmount += (this.DaysRented - 3) * 1.5;
+                    break;
+            }
+
+            return thisAmount;
+        }
     }
 }
